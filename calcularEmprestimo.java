824efece86df183c1e5a795d044e1c76b6ad3b2e
Jessica Monteiro <101065281+monteirojess@users.noreply.github.com>
2022-09-29 19:35:32 -0300

    Add files via upload

---
+++ calcularEmprestimo.java
@@ -0,0 +1,12 @@
+public class calcularEmprestimo {
+    public static void main(String[] args) {
+        Emprestimo nome = new Emprestimo();
+        nome.dadosEmprestimo("Jessica", 25, 1000,1.5,48);
+
+    }
+
+
+    }
+
+
+
