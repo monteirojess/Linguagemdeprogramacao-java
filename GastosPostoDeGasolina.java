06e9ada60bc7cbd9e6d287b347260db048b82cf8
Jessica Monteiro <101065281+monteirojess@users.noreply.github.com>
2022-10-05 11:29:40 -0300

    Update GastosPostoDeGasolina.java

--- GastosPostoDeGasolina.java
+++ GastosPostoDeGasolina.java
@@ -6,15 +6,15 @@ public class GastosPostoDeGasolina {
     public static void main(String[] args) {
         Scanner gasolina = new Scanner(System.in);
         System.out.print("Gasolina: ".toUpperCase());
-        float litro_gasolina = gasolina.nextFloat();
-        var PRECO_GASOLINA = 2.50;
-        var conta_total = litro_gasolina * PRECO_GASOLINA;
-        var CONSUMO_BEBIDAS = 2.00;
-        var PRECO_BEBIDAS = 3.00;
-        var GASTO_BEBIDAS_TOTAL = CONSUMO_BEBIDAS * PRECO_BEBIDAS;
-        var valor_total = conta_total + GASTO_BEBIDAS_TOTAL;
-        System.out.println("Litros Gasolina: " + litro_gasolina + " Valor por Litro: R$" + PRECO_GASOLINA + "= " + conta_total);
-        System.out.println("Quantidade de Bebidas: " + CONSUMO_BEBIDAS + " Valor das Bebidas: R$" + PRECO_BEBIDAS + "= " + GASTO_BEBIDAS_TOTAL);
-        System.out.println("Valor Total: R$" + valor_total);
+        float litroGasolina = gasolina.nextFloat();
+        var precoGasolina = 2.50;
+        var contaTotal = litroGasolina * precoGasolina;
+        var consumo_Bebidas = 2.00;
+        var precoBebidas = 3.00;
+        var gastoTotalBebida = consumo_Bebidas * precoBebidas;
+        var valor_Total = contaTotal + gastoTotalBebida;
+        System.out.println("Litros Gasolina: " + litroGasolina + " Valor por Litro: R$" + precoGasolina + "= " + contaTotal);
+        System.out.println("Quantidade de Bebidas: " + consumo_Bebidas + " Valor das Bebidas: R$" + precoBebidas + "= " + gastoTotalBebida);
+        System.out.println("Valor Total: R$" + valor_Total);
     }
 }
